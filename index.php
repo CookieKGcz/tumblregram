99b8c18cd49f66a8abab554b4d64160923914496
smekama <smekama@student.uzlabina.cz>
2023-03-18 18:59:47 +0100

    favicon

--- index.php
+++ index.php
@@ -51,6 +51,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CookieKGram</title>
+    <link rel="icon" type="image/x-icon" href="CCCC.ico">
 </head>
 
 <body>
