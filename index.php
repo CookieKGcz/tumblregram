7e6c22fb2121d18a7dae7c2ae747846e28123b1f
BrewsLee <ondratep@gmail.com>
2023-03-20 22:14:55 +0100

    tROCHU BROKEN

--- index.php
+++ index.php
@@ -9,28 +9,26 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CookieKGram</title>
-    <link rel="icon" type="image/x-icon" href="imgs/CCCCC.ico">
+    <link rel="icon" type="image/x-icon" href="imgs/logo.ico">
     <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
-    <div class="top-bar flex flex-dir-row border">
-        <img style="margin-left: 1rem;" src="imgs/výkresCCCC.svg" height="80%">
-        <!--<img src="imgs/CX.png" class="hund-perc">-->
-        <div class="top-bar buttons flex flex-dir-row justi-cont-right">
-        <?php
-        if (!isset($_SESSION["user"])) { ?>
-            
+    <div class="top-bar flex flex-dir-row just-cont-right">
+        <img style="margin-left: 1rem;" src="imgs/logo.svg" height="80%">
+        <h1 class="name">CookieGram</h1>
+        <div class="buttons flex flex-dir-row">
+            <?php
+            if (!isset($_SESSION["user"])) { ?>
                 <button id="sign-up" onclick="document.location='sign_up.php'">Sign up</button>
                 <button id="log-in" onclick="document.location='log_in.php'">Log in</button>
-            
             <?php
-        } else {
+            } else {
+                ?>
+                    <button id="log-out" onclick="document.location='log_out.php'">Log out</button>
+                <?php
+            }
             ?>
-                <button id="log-out" onclick="document.location='log_out.php'">Log out</button>
-            <?php
-        }
-        ?>
         </div>
     </div>
     <main>
