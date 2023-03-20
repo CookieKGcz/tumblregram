177d368e25d3f7cbe6552b8a05f867aacf08ec58
BrewsLee <ondratep@gmail.com>
2023-03-20 18:18:13 +0100

    Odstranil jsem zbytecny soubory

--- script.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -p "Enter commit message: " msg
-git add .
-git commit -m "$msg"
-git push
--- script.ps1
+++
@@ -1,4 +0,0 @@
-$msg = Read-Host -Prompt "Enter commit message"
-git add .
-git commit -m"$msg"
-git push
